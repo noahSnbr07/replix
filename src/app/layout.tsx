16422268819972e4229caa4999c06f67fc76c4bb
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-05-19 15:29:16 +0200

    inserted speed insights+analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { identity } from "@/assets/assets";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
 });
+
 export const metadata: Metadata = {
   title: identity.name,
   publisher: "EUL3",
@@ -22,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+      {/* Analytics + Speed Reports */}
+      <Analytics />
+      <SpeedInsights />
+
       <body
         className={`${montserrat.variable} antialiased`}
       >
