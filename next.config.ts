593a9236a578a1001f1508f853d700ef88a84ff1
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-05-23 23:56:23 +0200

    added remote hosts for placeholder svgs

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
 
   // enable cross origin security
   crossOrigin: "anonymous",
+
+  //allow placeholder images
+  images: {
+    remotePatterns: [new URL("https://placehold.co/400")],
+    dangerouslyAllowSVG: true,
+  }
 };
 
 export default nextConfig;
